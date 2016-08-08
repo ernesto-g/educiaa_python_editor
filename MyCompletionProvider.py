bf35a9b7dcf8e45420209125509e4eca5374a0fc
ernesto-g <ernestogigliotti@gmail.com>
2016-08-07 22:17:23 -0300

    autocomplete methods

--- MyCompletionProvider.py
+++ MyCompletionProvider.py
@@ -29,6 +29,25 @@ class MyCompletionProvider(gobject.GObject, gtksourceview2.CompletionProvider):
         self.keywordsPython.append("True")
         self.keywordsPython.append("False")
 
+        self.keywordsMethods =  {"LED":["on","off","intensity"],
+                                 "Switch":["callback","switch"],
+                                 "UART":["init","any","readchar","writechar","get_baudrate","read","readall","readline","readinto","write","deinit"],
+                                 "Pin":["init","value","low","high"],
+                                 "ExtInt" : ["line","enable","disable","swint"],
+                                 "DAC" : ["write","noise","triangle","write_timed"],
+                                 "Timer" : ["init","callback","interval","timeout","counter","deinit","freq","period","prescaler","source_freq"],
+                                 "PWM" : ["duty_cycle","set_frequency"],
+                                 "ADC" : ["read"],
+                                 "Keyboard" : ["get_char","get_matrix"],
+                                 "LCD" : ["clear","write","config_cursor","goto_xy"],
+                                 "EEPROM" : ["read_byte","read_int","read_float","write_byte","write_int","write_float","write","readall"],
+                                 "SPI" : ["write","read","readinto","write_readinto"],
+                                 "RTC" : ["datetime","alarm_datetime","read_bkp_reg","write_bkp_reg","calibration","alarm_disable","callback"],
+                                 "I2C" : ["write","read","readinto","slave_addr"],
+                                 "Instrument" : ["read_bit","write_bit","read_register","write_register","read_registers","write_registers"],
+                                 "Slave" : ["receive"]
+                               }
+
 
     def do_get_name(self):
         return 'MicroPython'
@@ -74,6 +93,12 @@ class MyCompletionProvider(gobject.GObject, gtksourceview2.CompletionProvider):
                 kwList = self.keywordsClasses[modName]
                 if word!=None:
                     kwList = [k for k in kwList if k.startswith(word)]
+            else:
+                data = self.__findObjectClass(modName,context)
+                className = data[1]
+                if className in self.keywordsMethods:
+                    kwList = self.keywordsMethods[className]
+
 
         if type==TYPE_4 : # =: show classes and modules
             kwList = self.keywordsModules
@@ -136,3 +161,43 @@ class MyCompletionProvider(gobject.GObject, gtksourceview2.CompletionProvider):
         iterStart.backward_word_starts(1)
         left_text = buf.get_text(iterStart, end_iter, True)
         return -1,left_text
+
+
+    def __findObjectClass(self,objName,context):
+        end_iter = context.get_iter()
+        if end_iter:
+            buf = end_iter.get_buffer()
+            mov_iter = end_iter.copy()
+ 
+            while True:
+
+                if mov_iter.backward_search(objName+' =', gtk.TEXT_SEARCH_VISIBLE_ONLY):
+                    mov_iter2, _ = mov_iter.backward_search(objName+' =', gtk.TEXT_SEARCH_VISIBLE_ONLY,limit=None)
+                    mov_iter3 = mov_iter2.copy()
+                    mov_iter3.forward_line()
+                    left_text = buf.get_text(mov_iter2, mov_iter3, True)
+                    parts = left_text.split(" ")
+                    #print(parts)
+                    if len(parts)>=2:
+                        if parts[1]=="==":
+                            mov_iter = mov_iter2
+                            continue
+                        if len(parts)<3:
+                            mov_iter = mov_iter2
+                            continue                        
+                        s = parts[2]
+                        #print(s)
+                        #check for module.class() format
+                        parts = s.split(".")
+                        if len(parts)>1:
+                            # module.class()
+                            return parts[0],parts[1].split("(")[0]
+                        else:
+                            # only class
+                            return None,s.split("(")[0]
+
+                    mov_iter = mov_iter2
+                else:
+                    break
+
+        return None,None
