9d642c807188bffe6ed7c89ea4c4e2fc3e499520
ernesto-g <ernestogigliotti@gmail.com>
2016-08-31 21:47:12 -0300

    tips btn events

--- tips/TipsWindow.py
+++ tips/TipsWindow.py
@@ -40,10 +40,22 @@ class TipsWindow():
 		self.window.show_all()
 		self.window.connect("delete_event", self.__closeBtnEvent)	
 		self.window.connect("destroy", self.__closeBtnEvent)	
+
+		#Button Next
+		self.buttonOk = builder.get_object("btnNext")
+		self.buttonOk.connect("clicked", self.__nextEvent, None)
+
+		#Button Close
+		self.buttonOk = builder.get_object("btnClose")
+		self.buttonOk.connect("clicked", self.__closeBtnEvent, None)
+
+
 		
-	def __closeEvent(self,a1,a2):		
+	def __nextEvent(self,a1,a2):
+		print("next!!")
+
+
+	def __closeBtnEvent(self,a1,a2):		
 		self.window.destroy()
 		self.tipsWindowCloseEventCallback()
 		
-	def __closeBtnEvent(self,*args):
-		return True
